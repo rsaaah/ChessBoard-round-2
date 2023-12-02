0406417c3f4fe75f364574c807dc269ad2a8cd08
Ken <Ken@DESKTOP-D072FBA>
2023-12-02 13:34:00 -0500

    Add project files.

---
+++ ChessBoard round 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoard_round_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+                
+            Console.WriteLine("Please select a number");
+            string userLines = Console.ReadLine();
+            int.TryParse(userLines, out int userBoard);
+
+            for (int col = 0; col < userBoard; col++) 
+            {
+                Console.WriteLine("®");
+                for (int row = 0; row < userBoard; row++ )
+                {
+                    Console.WriteLine("Δ");
+                }
+            }
+
+                Console.WriteLine("Δ");
+             
+            
+        }
+    }
+}
